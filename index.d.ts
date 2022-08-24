2b1158bcfc85e1674b1a00b498e1c678fb10b24d
Grant Colestock <grant@rubberduck.software>
2022-08-24 13:56:50 -0500

    fix: export type

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,27 @@
 type test = (name: string, fn: () => void | Promise<void>) => void;
 
-export declare function describe(
-  headline: string,
-  fn: () => void
-): Promise<boolean>;
+type Describe = (headline: string, fn: () => void) => Promise<boolean>;
+type It = test & { only: test; skip: test };
+type BeforeAll = (fn: () => void | Promise<void>) => void;
+type AfterAll = (fn: () => void | Promise<void>) => void;
+type BeforeEach = (fn: () => void | Promise<void>) => void;
+type AfterEach = (fn: () => void | Promise<void>) => void;
 
-export declare const it: test & { only: test; skip: test };
-export declare function beforeAll(fn: () => void | Promise<void>): void;
-export declare function afterAll(fn: () => void | Promise<void>): void;
-export declare function beforeEach(fn: () => void | Promise<void>): void;
-export declare function afterEach(fn: () => void | Promise<void>): void;
+export declare const describe: Describe;
+export declare const it: It;
+
+export declare const beforeAll: BeforeAll;
+export declare const afterAll: AfterAll;
+export declare const beforeEach: BeforeEach;
+export declare const afterEach: AfterEach;
+
+interface BearTest {
+  describe: Describe;
+  it: It;
+  beforeAll: BeforeAll;
+  afterAll: AfterAll;
+  beforeEach: BeforeEach;
+  afterEach: AfterEach;
+}
+
+export default BearTest;
