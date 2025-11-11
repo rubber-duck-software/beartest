aacbf896fe1c1767ec35b03eb726d72914857eb0
Grant Colestock <grant@rubberduck.software>
2025-11-11 01:15:45 -0600

    7.0.2

--- cli.js
+++ cli.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 const { run } = require('./beartest')
 const fs = require('node:fs')
+const path = require('node:path')
 
 async function cli() {
   const globStr = process.argv[2] || '**/*.test.*'
 
   const files = await Array.fromAsync(fs.promises.glob(globStr))
 
-  for await (let event of run({ files: files })) {
+  for await (let event of run({ files: files.map((f) => path.resolve(f)) })) {
     const prefix = '  '.repeat(event.data.nesting)
     if (event.type === 'test:start' && event.data.type === 'suite') {
-      process.stdout.write(`\x1b[36m${prefix}${event.data.name} \n\x1b[0m`)
+      if (path.isAbsolute(event.data.name)) {
+        console.log(`\x1b[36m${prefix}${path.parse(event.data.name).name} (${path.relative('./', event.data.name)})\x1b[0m`)
+      } else {
+        console.log(`\x1b[36m${prefix}${event.data.name}\x1b[0m`)
+      }
     } else if (event.type === 'test:pass' && event.data.details.type === 'test' && !event.data.skip) {
       process.stdout.write(`\x1b[32m${prefix}✓\x1b[0m\x1b[90m ${event.data.name}\n\x1b[0m`)
     } else if (event.type === 'test:fail' && event.data.details.type === 'test') {
